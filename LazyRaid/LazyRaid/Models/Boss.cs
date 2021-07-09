7c3c467c6db0b1bae84eb4bf9a62cde767c2c7dd
zinnin <34364024+zinnin@users.noreply.github.com>
2021-07-08 23:39:16 -0700

    HERE WE GO

---
+++ LazyRaid/LazyRaid/Models/Boss.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.ObjectModel;
+
+namespace LazyRaid.Models
+{
+    class Boss
+    {
+        public string Name { get; set; }
+        public ObservableCollection<BossAbility> Abilities { get; set; } 
+        public BossSchedule Schedule { get; set; }
+    }
+}
