c49d5d0b4f72bbee42ae33b6c7840837cb67567d
zinnin <34364024+zinnin@users.noreply.github.com>
2021-07-09 21:40:10 -0700

    FUCK REGIONS

---
+++ LazyRaid/LazyRaid/Models/UserData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazyRaid.Models
+{
+    public class UserData
+    {
+        public Boss Boss { get; set; }
+        public Raid Raid { get; set; }
+    }
+}
--- LazyRaid/LazyRaid/ViewModels/LazyRaid_VM.cs
+++ LazyRaid/LazyRaid/ViewModels/LazyRaid_VM.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LazyRaid.Models;
 using Prism.Commands;
 using Prism.Mvvm;
 
@@ -12,15 +13,7 @@ namespace LazyRaid
 {
     class LazyRaid_VM : BindableBase
     {
-        #region Local Variables
-
-        #endregion
-
-        #region DelegateCommands
-
-        #endregion
-
-        #region Constructors and Load
+        public UserData UserData { get; set; }
         public LazyRaid_VM()
         {
             CreateCommands();
@@ -30,6 +23,94 @@ namespace LazyRaid
         {
 
         }
-        #endregion
+
+        private Task AutoSchedulerTask { get; set; }
+        public BossTime FirstPassScheduleTimeStamp { get; set; } = new BossTime();
+        public bool ReverseAutoSchedulers { get; set; }
+        public BossSchedulerSettings Settings { get; set; }
+
+        public async Task RunAutoScheduler()
+        {
+            if (AutoSchedulerTask == null || AutoSchedulerTask.IsCompleted)
+            {
+                AutoSchedulerTask = Task.Run(AutoSchedule);
+                await AutoSchedulerTask;
+            }
+        }
+
+        private void AutoSchedule()
+        {
+            UserData.Boss.Events.Sort((a, b) => { return a.Timestamp.CompareTo(b.Timestamp); });
+            foreach (BossEvent bossEvent in UserData.Boss.Events)
+            {
+
+            }
+        }
+
+        public void Load(Boss boss)
+        {
+            UserData.Boss = boss;
+
+        }
+
+        public void NewAbility(string abilityName)
+        {
+            if (BossLoaded())
+            {
+                BossAbility bossAbility = new BossAbility
+                {
+                    Name = abilityName,
+                };
+
+                UserData.Boss.Abilities.Add(bossAbility);
+                UserData.Boss.AbilityPriorities.Add(bossAbility, new OC<CounterAbilityUserPriority>());
+            }
+        }
+
+        public void RemoveAbility(BossAbility ability)
+        {
+            if (BossLoaded() && UserData.Boss.Abilities.Contains(ability))
+            {
+                UserData.Boss.Abilities.Remove(ability);
+                UserData.Boss.AbilityPriorities.Remove(ability);
+            }
+        }
+
+        private void EnforceAbilityPriorityDefinitons()
+        {
+            if (BossLoaded())
+            {
+                foreach (KeyValuePair<BossAbility, OC<CounterAbilityUserPriority>> abilityPriorityDefinition in UserData.Boss.AbilityPriorities)
+                {
+
+                }
+            }
+        }
+
+        private OC<PlayerAbility> GetAvailableCounterList(BossAbility bossAbility)
+        {
+            OC<PlayerAbility> abilityCounters = new OC<PlayerAbility>();
+
+            foreach (Player player in UserData.Raid.Players)
+            {
+                if (player.CurrentSpecialization != null)
+                {
+                    foreach (PlayerAbility playerAbility in player.CurrentSpecialization.Abilities)
+                    {
+                        if (playerAbility.IsCounter(bossAbility))
+                        {
+                            abilityCounters.Add(playerAbility);
+                        }
+                    }
+                }
+            }
+
+            return abilityCounters;
+        }
+
+        private bool BossLoaded()
+        {
+            return (UserData.Boss != null);
+        }
     }
 }
